566ad1c2df9d14386bfe4c8c2be071946e99a307
angel <debian789@gmail.com>
2014-01-14 15:34:57 -0500

    filtro de busqueda en mis codigos

--- apps/prueba/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns ('apps.prueba.views',
-	url(r'^prueba/$','views_prueba', name = 'prueba'),
-)
--- static/js/main.2739d9e90008.js
+++
@@ -1,1 +0,0 @@
-$('#myTab a:last').tab('show'); 
