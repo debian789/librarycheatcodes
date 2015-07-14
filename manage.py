8550d8215e1084386ac056f2078b03a87ba14886
Miguel Suescun <debian789@gmail.com>
2015-07-14 18:10:11 -0500

    Estructuracion del proyecto con la version 1.8.3 de django

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "administrador_codigo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "librarycheatcodes.settings")
 
     from django.core.management import execute_from_command_line
 
