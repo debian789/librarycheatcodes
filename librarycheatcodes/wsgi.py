07ae144b7e21f15b00fe5cec15ade6d2f7f16a81
Miguel Suescun <debian789@gmail.com>
2015-10-28 13:30:13 -0500

    prueba de heroku

--- librarycheatcodes/wsgi.py
+++ librarycheatcodes/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "librarycheatcodes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "librarycheatcodes.settings.development")
 
 application = get_wsgi_application()
